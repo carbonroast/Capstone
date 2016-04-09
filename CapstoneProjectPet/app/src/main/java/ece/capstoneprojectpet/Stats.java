aa431a1223401bef28038d9bcb793ea423bb94f1
Yangarang <jonyang7777@gmail.com>
2016-04-09 18:32:50 -0400

    Stats update
    added extend pet on stats.java

--- CapstoneProjectPet/app/src/main/java/ece/capstoneprojectpet/Stats.java
+++ CapstoneProjectPet/app/src/main/java/ece/capstoneprojectpet/Stats.java
@@ -8,14 +8,23 @@ import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 import android.os.Handler;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 /**
  * Created by Jonathan on 3/29/2016.
  */
-public class Stats {
-
+public class Stats extends Pet {
     int hunger;
     int happy;
     int intel;
+
+    int energy;
+    int health;
+    int aggression;
+    int bathroom;
 
     // default constructor
     public Stats() {
@@ -23,34 +32,44 @@ public class Stats {
         hunger = 5;
         happy = 5;
         intel = 5;
+        energy = 5;
+        health = 5;
+        aggression =5;
+        bathroom = 5;
     }
 
     // set constructor stats
-    public Stats(int a, int b, int c) {
+    public Stats(int a, int b, int c, int d, int e, int f, int g) {
         hunger = a;
         happy = b;
         intel = c;
+        energy = d;
+        health = e;
+        aggression = f;
+        bathroom = g;
     }
 
-    public void addHunger() {
-        hunger++;
+    public void changeHunger(int number) {
+        hunger= hunger + number;
     }
-    public void subHunger() {
-        hunger--;
+    public void changeHappy(int number) {
+        happy= happy + number;
     }
-    public void addHappy() {
-        happy++;
+    public void changeIntel(int number){
+        intel = intel + number;
     }
-    public void subHappy() {
-        happy--;
+    public void changeEnergy(int number){
+        energy = energy + number;
     }
-    public void addIntel() {
-        intel++;
+    public void changeHealth(int number){
+        health = health + number;
     }
-    public void subIntel() {
-        intel--;
+    public void changeAggression(int number){
+        aggression = aggression + number;
     }
-
+    public void changeBathroom(int number){
+        bathroom = bathroom + number;
+    }
 
     public int getHunger() {
         return hunger;
@@ -61,6 +80,51 @@ public class Stats {
     public int getIntel() {
         return intel;
     }
+    public int getEnergy() {
+        return energy;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public int getAggression() {
+        return aggression;
+    }
+    public int getBathroom() {
+        return bathroom;
+    }
 
+    // write log to file
+    private void writeFile(String log) {
+        try {
+            FileOutputStream file = openFileOutput("PETstats.txt", MODE_APPEND);
+            OutputStreamWriter outputWriter = new OutputStreamWriter(file);
+            outputWriter.write(log);
+            outputWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // read log from file
+    private String readFile() {
+        try {
+            char[] buffer= new char[100];
+            String log = "";
+            FileInputStream file = openFileInput("PETstats.txt");
+            InputStreamReader InputRead = new InputStreamReader(file);
+            int read;
+            while ((read = InputRead.read(buffer)) > 0) {
+                // char to string
+                String readstring=String.copyValueOf(buffer,0,read);
+                log = log + readstring;
+            }
+            InputRead.close();
+            return log;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
 
 }
+
